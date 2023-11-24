2d0272674fb06003ae1405a4ba637dea0d45c614
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-24 23:02:07 +0300

    Remove eslint extention from the nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // eslint-disable-next-line no-undef
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  extends: ['@nuxtjs/eslint-config']
+  devtools: { enabled: true }
 })
