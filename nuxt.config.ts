4d73a985fb68d66fd1d3f99dbf3446d2a7d0de67
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-24 22:58:49 +0300

    Fix lint issues in the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["@nuxtjs/eslint-config"],
+  extends: ['@nuxtjs/eslint-config']
 })
