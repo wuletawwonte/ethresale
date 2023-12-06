bf8e107c49d0deedb4b6a37b4912a78d7f305924
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-06 16:47:16 +0300

    Add categories in the hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -17,21 +17,41 @@
       </button>
     </div>
     <div class="hero__categories">
-      <ul>
-        <li>
-          <a href="https://www.facebook.com/">
-            <i class="fab fa-facebook">Facebook</i>
-          </a>
-        </li>
-      </ul>
+      <p>Main Second Hand Products Categories</p>
+      <div class="hero__categories__wrapper">
+        <NuxtLink v-for="category in categories" :key="category.id" :to="category.link">
+          <Icon :name="category.icon" size="1.7rem" />
+          <span>{{ category.name }}</span>
+        </NuxtLink>
+      </div>
     </div>
   </section>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        { id: 1, name: 'Mobile Phones', link: 'categories/mobilephones', icon: 'fa-solid:mobile' },
+        { id: 2, name: 'Cars', link: 'categories/cars', icon: 'fa-solid:car' },
+        { id: 3, name: 'Motorcycles', link: 'categories/motorcycles', icon: 'fa-solid:motorcycle' },
+        { id: 4, name: 'Houses', link: 'categories/houses', icon: 'fa-solid:home' },
+        { id: 5, name: 'TV-Video-Audio', link: 'categories/tv', icon: 'fa-solid:tv' },
+        { id: 6, name: 'Tablets', link: 'categories/tablets', icon: 'fa-solid:tablet' },
+        { id: 7, name: 'Land & Plots', link: 'categories/landandplots', icon: 'fa6-solid:landmark' },
+        { id: 8, name: 'Games', link: 'categories/videogames', icon: 'fa-solid:gamepad' },
+        { id: 18, name: 'Computers', link: 'categories/computers', icon: 'fa-solid:desktop' }
+      ]
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
   .hero {
     display: flex;
-    height: 45vh;
+    min-height: 45vh;
     background-color: #eee;
     color: #fff;
     font-weight: 700;
@@ -107,6 +127,43 @@
 
     &__categories {
       flex-basis: 50%;
+      display: flex;
+      gap: 1rem;
+      flex-direction: column;
+      padding: 3rem 1rem 1rem 1rem;
+
+      p {
+        font-size: 1rem;
+        font-weight: 100;
+        color: var(--text-color);
+      }
+
+      &__wrapper {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 0.5rem;
+
+        a {
+          padding: 0.3rem;
+          font-size: 0.8rem;
+          font-weight: 200;
+          text-decoration: none;
+          color: var(--text-color);
+          border: var(--input-border);
+          border-radius: var(--border-radius);
+          display: flex;
+          align-items: center;
+          gap: 0.5rem;
+          min-width: 90px;
+          transition: all 0.2s ease-in-out;
+
+          &:hover {
+            background-color: var(--primary-color);
+            color: #fff;
+            padding-left: 0.5rem;
+          }
+        }
+      }
     }
   }
 </style>
