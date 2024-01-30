b0fd98c67d01d72358192245f0e9607d3214e4ac
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-01-30 16:18:40 +0300

    add hero left section

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <AppHeader />
+    <Header />
     <NuxtPage />
     <AppFooter />
   </NuxtLayout>
--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -45,24 +45,36 @@ const categories = ref([
 </script>
 
 <template>
-  <section class="hero">
-    <div class="hero__form">
-      <h3>Buy and Sell Second Hand or Used Products</h3>
-      <form action="">
-        <DropDownInput />
-        <input type="search" placeholder="Name" />
-        <button><Icon name="fa-solid:search" size="1.1rem" /></button>
+  <section class="bg-neutral-content px-48 flex min-h-72">
+    <div
+      class="basis-1/2 flex flex-col items-start gap-4 border-r-2 px-8 py-5 prose"
+    >
+      <h3 class="text-sky-700">Buy and Sell Second Hand or Used Products</h3>
+      <form action="" class="flex w-full p-4 bg-sky-600 rounded-md">
+        <select class="select select-bordered select-sm w-full max-w-xs mr-2">
+          <option disabled selected>City ...</option>
+          <option>Arbaminch</option>
+          <option>Hawassa</option>
+        </select>
+        <input
+          type="search"
+          placeholder="Search ..."
+          class="input input-bordered input-sm w-full max-w-xs rounded-e-none"
+        />
+        <button class="btn btn-sm rounded-s-none rounded-e-full">
+          <Icon name="fa-solid:search" size="1rem" />
+        </button>
       </form>
-      <button>
+      <button class="btn btn-sm btn-block flex justify-start items-center">
         <Icon name="material-symbols:box-edit-outline" size="1.2rem" />
         <span>Post Ad to Sell Used Second Hand Product</span>
       </button>
-      <button>
+      <button class="btn btn-sm btn-block flex justify-start items-center">
         <Icon name="material-symbols:list-rounded" size="1.3rem" />
         <span>Find and Buy All Second Hand Products for Sale</span>
       </button>
     </div>
-    <div class="hero__categories">
+    <div class="basis-1/2">
       <p>Main Second Hand Products Categories</p>
       <div class="hero__categories__wrapper">
         <NuxtLink
