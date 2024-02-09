6cba1a5552a65145c3cb67aa5adc447ea4e0630e
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-02-09 14:38:13 +0300

    change file upload form input

--- pages/my-items/add.vue
+++ pages/my-items/add.vue
@@ -11,8 +11,30 @@
         <div class="mb-1 md:mb-0 md:w-1/3 prose">
           <label>Product Photos</label>
         </div>
+
         <div class="md:w-2/3 md:flex-grow">
-          <button class="btn">Add a Picture</button>
+          <div class="flex items-center justify-center w-full">
+            <label
+              for="dropzone-file"
+              class="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div class="flex flex-col items-center justify-center py-5">
+                <icon
+                  name="octicon:cloud-upload"
+                  size="3rem"
+                  class="text-gray-400 dark:text-gray-500"
+                ></icon>
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span class="font-semibold">Click to upload</span> or drag and
+                  drop
+                </p>
+                <p class="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG or GIF (MAX. 800x400px)
+                </p>
+              </div>
+              <input id="dropzone-file" type="file" class="hidden" />
+            </label>
+          </div>
         </div>
       </div>
 
