9d6aad21e6e48f4ddb653bf83911ed8f024dbba5
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-05 17:00:55 +0300

    replace dropdown with my items menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@ const logout = async () => {
     console.log(error);
   }
 
-  navigateTo('/login');
+  navigateTo("/login");
 };
 </script>
 
@@ -22,51 +22,37 @@ const logout = async () => {
         </div>
         <ul
           tabindex="0"
-          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
         >
           <li><nuxt-link to="/"> Home </nuxt-link></li>
           <li><nuxt-link to="/about"> About </nuxt-link></li>
           <li><nuxt-link to="/contact"> Contact </nuxt-link></li>
-          <li>
-            <a>Manage Items</a>
-            <ul class="p-2">
-              <li><nuxt-link to="/my-items">My Items</nuxt-link></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
+          <li><nuxt-link to="/my-items">My Items</nuxt-link></li>
         </ul>
       </div>
       <nuxt-link to="/" class="btn btn-ghost text-xl">
         <img src="/logo.svg" alt="Nuxt Logo" class="w-8" />
-        <span class="hidden lg:flex">{{ $t('ethresale') }}</span>
+        <span class="hidden lg:flex">{{ $t("ethresale") }}</span>
       </nuxt-link>
     </div>
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
         <li>
-          <nuxt-link to="/about"> {{ $t('about_title') }} </nuxt-link>
+          <nuxt-link to="/about"> {{ $t("about_title") }} </nuxt-link>
         </li>
         <li>
-          <nuxt-link to="/contact"> {{ $t('contact_title') }} </nuxt-link>
+          <nuxt-link to="/contact"> {{ $t("contact_title") }} </nuxt-link>
         </li>
-        <li v-if="user">
-          <details>
-            <summary>Manage Items</summary>
-            <ul class="p-2">
-              <li><nuxt-link to="/my-items">My Items</nuxt-link></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
+        <li v-if="user"><nuxt-link to="/my-items">My Items</nuxt-link></li>
       </ul>
     </div>
     <div class="navbar-end gap-2">
-      <button class="btn btn-sm btn-ghost btn-circle">
+      <button class="btn btn-circle btn-ghost btn-sm">
         <icon name="ph:magnifying-glass-bold" size="1.3rem" />
       </button>
       <ThemeSwitcher />
       <div class="dropdown dropdown-end">
-        <div tabindex="0" role="button" class="btn btn-sm btn-ghost btn-circle">
+        <div tabindex="0" role="button" class="btn btn-circle btn-ghost btn-sm">
           <div class="indicator">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -82,15 +68,15 @@ const logout = async () => {
                 d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
               />
             </svg>
-            <span class="badge badge-sm indicator-item">8</span>
+            <span class="badge indicator-item badge-sm">8</span>
           </div>
         </div>
         <div
           tabindex="0"
-          class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+          class="card dropdown-content card-compact z-[1] mt-3 w-52 bg-base-100 shadow"
         >
           <div class="card-body">
-            <span class="font-bold text-lg">8 Items</span>
+            <span class="text-lg font-bold">8 Items</span>
             <span class="text-info">Subtotal: $999</span>
             <div class="card-actions">
               <button class="btn btn-primary btn-block">View cart</button>
@@ -100,11 +86,11 @@ const logout = async () => {
       </div>
 
       <nuxt-link v-if="!user" to="/login" class="btn btn-sm">
-        {{ $t('Sign in') }}
+        {{ $t("Sign in") }}
       </nuxt-link>
       <LangSwitcher />
       <div v-if="user" class="dropdown dropdown-end">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
           <div class="w-10 rounded-full">
             <img
               alt="Tailwind CSS Navbar component"
@@ -114,7 +100,7 @@ const logout = async () => {
         </div>
         <ul
           tabindex="0"
-          class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
         >
           <li>
             <a class="justify-between">
@@ -124,7 +110,7 @@ const logout = async () => {
           </li>
           <li><a>Settings</a></li>
           <li>
-            <a @click.prevent="logout">{{ $t('Logout') }}</a>
+            <a @click.prevent="logout">{{ $t("Logout") }}</a>
           </li>
         </ul>
       </div>
