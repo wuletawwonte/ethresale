422aaf4eb2f758d16c51e7e5a3aaa703383be0dd
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-06 15:56:57 +0300

    change components nuxt config
    - the change registers the components using component name not the path

---
+++ src/components/input-and-actions/VueInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface InputProps {
+  label?: string;
+  value?: string;
+}
+
+const props = withDefaults(defineProps<InputProps>(), {
+  label: "TestLabel",
+  value: "TestValue",
+});
+</script>
+
+<template>
+  <label>
+    {{ props.label }}
+  </label>
+  <div>
+    <input type="text" :value="props.value" />
+  </div>
+</template>
--- src/pages/my-items/add.vue
+++ src/pages/my-items/add.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 const selectedFiles = ref([] as File[]);
-const name = ref('');
-const description = ref('');
-const category = ref('');
-const price = ref('');
-const city = ref('');
+const name = ref("");
+const description = ref("");
+const category = ref("");
+const price = ref("");
+const city = ref("");
 
 // saves the item to the database
 function saveItem(values: any) {
-  console.log('Save Item', values);
+  console.log("Save Item", values);
 }
 
 // adds the image to the selectedFiles array
@@ -19,7 +19,7 @@ function onFileChange(event: Event) {
       const currFile: File = files.item(i)!;
 
       const isDuplicate = selectedFiles.value.some(
-        (file: File) => file.name === currFile.name
+        (file: File) => file.name === currFile.name,
       );
 
       if (!isDuplicate) {
@@ -33,7 +33,7 @@ function onFileChange(event: Event) {
 function removeImage(index: string) {
   if (selectedFiles) {
     const newFiles = [...selectedFiles.value!].filter(
-      (file: File) => file.name !== index
+      (file: File) => file.name !== index,
     );
     selectedFiles.value = newFiles;
   }
@@ -48,15 +48,15 @@ function loadItemImage(imageItem: File): string {
 
 function isRequired(value: string) {
   if (!value) {
-    return 'This field is required';
+    return "This field is required";
   }
 
   return true;
 }
 
 function notEmpty(value: string) {
-  if (value.trim() === '') {
-    return 'This field is required';
+  if (value.trim() === "") {
+    return "This field is required";
   }
 
   return true;
@@ -64,25 +64,26 @@ function notEmpty(value: string) {
 </script>
 
 <template>
-  <div class="flex flex-col lg:px-[12%] my-4 mx-4 lg:mx-0">
-    <div class="flex justify-between items-center px-4">
-      <h2 class="text-2xl font-normal">{{ $t('Add Item') }}</h2>
+  <div class="mx-4 my-4 flex flex-col lg:mx-0 lg:px-[12%]">
+    <div class="flex items-center justify-between px-4">
+      <h2 class="text-2xl font-normal">{{ $t("Add Item") }}</h2>
     </div>
 
     <VeeForm @submit="saveItem">
+      <VueInput />
       <div
-        class="flex flex-col my-4 gap-6 shadow-sm p-4 rounded-xl bg-base-100"
+        class="my-4 flex flex-col gap-6 rounded-xl bg-base-100 p-4 shadow-sm"
       >
         <div class="text-gray-700 md:flex">
-          <div class="mb-1 md:mb-0 md:w-1/3 prose">
+          <div class="prose mb-1 md:mb-0 md:w-1/3">
             <label>Product Photos</label>
           </div>
 
-          <div class="md:w-2/3 md:flex-grow flex flex-col gap-4">
-            <div class="flex items-center justify-center w-full">
+          <div class="flex flex-col gap-4 md:w-2/3 md:flex-grow">
+            <div class="flex w-full items-center justify-center">
               <label
                 for="dropzone-file"
-                class="flex flex-col items-center justify-center w-full h-34 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                class="h-34 dark:hover:bg-bray-800 flex w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
               >
                 <div class="flex flex-col items-center justify-center py-4">
                   <icon
@@ -111,15 +112,15 @@ function notEmpty(value: string) {
             </div>
             <div
               v-if="selectedFiles"
-              class="grid grid-cols-2 gap-4 mt-4 md:grid-cols-6"
+              class="mt-4 grid grid-cols-2 gap-4 md:grid-cols-6"
             >
               <div
                 v-for="imageItem in selectedFiles"
                 :key="imageItem.name"
-                class="relative md:h-24 flex flex-col items-center overflow-hidden text-center border border-base-300 rounded cursor-pointer select-none hover:shadow hover:border-base-200"
+                class="relative flex cursor-pointer select-none flex-col items-center overflow-hidden rounded border border-base-300 text-center hover:border-base-200 hover:shadow md:h-24"
               >
                 <button
-                  class="btn btn-circle btn-xs absolute top-1 right-1 z-50 bg-base-100"
+                  class="btn btn-circle btn-xs absolute right-1 top-1 z-50 bg-base-100"
                   type="button"
                   @click="removeImage(imageItem.name)"
                 >
@@ -130,11 +131,11 @@ function notEmpty(value: string) {
                   ></icon>
                 </button>
                 <img
-                  class="inset-0 z-0 object-cover w-full h-full border-4 border-base-100"
+                  class="inset-0 z-0 h-full w-full border-4 border-base-100 object-cover"
                   :src="loadItemImage(imageItem)"
                 />
                 <span
-                  class="absolute bg-base-100 p-1 bg-opacity-80 bottom-0 z-50 text-xs text-base-content dark:text-gray-400"
+                  class="absolute bottom-0 z-50 bg-base-100 bg-opacity-80 p-1 text-xs text-base-content dark:text-gray-400"
                 >
                   {{ imageItem.name.substring(0, 15) }}
                 </span>
@@ -147,7 +148,7 @@ function notEmpty(value: string) {
         <hr class="border-base-200" />
 
         <div class="text-gray-700 md:flex">
-          <div class="mb-1 md:mb-0 md:w-1/3 prose">
+          <div class="prose mb-1 md:mb-0 md:w-1/3">
             <label>Product Name</label>
           </div>
           <div class="md:w-2/3 md:flex-grow">
@@ -156,14 +157,13 @@ function notEmpty(value: string) {
               type="text"
               placeholder="Product name"
               name="name"
-              :rules="isRequired || notEmpty"
             />
             <VeeErrorMessage name="name" class="mt-2 text-sm text-red-500" />
           </div>
         </div>
 
         <div class="text-gray-700 md:flex">
-          <div class="mb-1 md:mb-0 md:w-1/3 prose">
+          <div class="prose mb-1 md:mb-0 md:w-1/3">
             <label>Description</label>
           </div>
           <div class="md:w-2/3 md:flex-grow">
@@ -195,7 +195,7 @@ function notEmpty(value: string) {
         </div>
 
         <div class="md:flex">
-          <div class="mb-1 md:mb-0 md:w-1/3 prose">
+          <div class="prose mb-1 md:mb-0 md:w-1/3">
             <label>Price</label>
           </div>
           <div class="md:w-2/3 md:flex-grow">
@@ -204,22 +204,22 @@ function notEmpty(value: string) {
                 <VeeField
                   name="price"
                   type="number"
-                  class="input input-bordered join-item w-80 focus:outline-none appearance-none"
+                  class="input join-item input-bordered w-80 appearance-none focus:outline-none"
                   placeholder="Price"
                   v-model="price"
                 />
               </div>
               <div
-                class="join-item border border-base-300 flex items-center px-4 bg-base-100"
+                class="join-item flex items-center border border-base-300 bg-base-100 px-4"
               >
-                <span class="text-base-content text-sm">ETB</span>
+                <span class="text-sm text-base-content">ETB</span>
               </div>
             </div>
           </div>
         </div>
 
         <div class="text-gray-700 md:flex">
-          <div class="mb-1 md:mb-0 md:w-1/3 flex flex-col prose">
+          <div class="prose mb-1 flex flex-col md:mb-0 md:w-1/3">
             <label>City</label>
             <label class="text-xs opacity-60 hover:opacity-80"
               >Where are you located?{{ city }}</label
@@ -231,7 +231,7 @@ function notEmpty(value: string) {
                 <VeeField
                   as="select"
                   name="city"
-                  class="select select-bordered join-item"
+                  class="join-item select select-bordered"
                   placeholder="City"
                 >
                   <option value="" selected>City</option>
@@ -247,7 +247,7 @@ function notEmpty(value: string) {
 
         <div class="text-gray-700 md:flex">
           <div class="mb-1 md:mb-0 md:w-1/3"></div>
-          <div class="md:w-2/3 md:flex-grow flex gap-4">
+          <div class="flex gap-4 md:w-2/3 md:flex-grow">
             <button type="submit" class="btn btn-primary">Publish now</button>
             <button class="btn">Save Draft</button>
             <button type="reset" class="btn btn-secondary">Reset</button>
