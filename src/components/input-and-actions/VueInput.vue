61e649380639d902200f44441757b99d9607a246
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-23 15:48:29 +0300

    Add suffix and prefix property in input components (#103)
    * add optional props of suffix and prefix in VueInput component
    
    * add optional suffix string prop
    
    * add optional prefix string prop

--- src/components/input-and-actions/VueInput.vue
+++ src/components/input-and-actions/VueInput.vue
@@ -13,6 +13,8 @@ interface InputProps {
   placeholder?: string;
   type?: string;
   name?: string;
+  prefix?: string;
+  suffix?: string;
   orientation?: "vertical" | "horizontal";
   id: string;
   required?: boolean;
@@ -23,6 +25,8 @@ const props = withDefaults(defineProps<InputProps>(), {
   label: "",
   sublabel: "",
   placeholder: "",
+  prefix: "",
+  suffix: "",
   orientation: "horizontal",
   name: "",
   required: false,
@@ -47,16 +51,33 @@ const onInput = (e: Event) => {
       {{ props.label }}
     </Label>
     <div :class="['flex-grow', { 'md:w-2/3': orientation === 'horizontal' }]">
-      <input
-        :type="type"
-        :id="id"
-        ref="inputRef"
-        :name="name"
-        class="input input-bordered w-full"
-        :value="props.modelValue"
-        :placeholder="props.placeholder"
-        @input="onInput"
-      />
+      <div class="join">
+        <div
+          v-if="prefix"
+          class="join-item flex items-center border border-base-300 bg-base-100 px-4"
+        >
+          <span class="text-sm text-base-content"> {{ prefix }} </span>
+        </div>
+
+        <div>
+          <input
+            :type="type"
+            :id="id"
+            ref="inputRef"
+            :name="name"
+            class="input join-item input-bordered w-full appearance-none focus:outline-none"
+            :value="props.modelValue"
+            :placeholder="props.placeholder"
+            @input="onInput"
+          />
+        </div>
+        <div
+          v-if="suffix"
+          class="join-item flex items-center border border-base-300 bg-base-100 px-4"
+        >
+          <span class="text-sm text-base-content">{{ suffix }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
