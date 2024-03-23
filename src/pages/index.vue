f87e0dc3c7446003268288b6dbe7421004b9c913
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-23 23:27:11 +0300

    Show daisyui skeleton when loading items in the homepage (#109)
    * add skeleton boilerplate when items are loading
    
    * style the skeleton component
    
    * repeat skeleton content few times

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 import type { Database } from "~/types/supabase";
 const client = useSupabaseClient<Database>();
 
-const { data: items } = await useAsyncData(
+const { data: items, pending } = await useAsyncData(
   "items",
   async () => await client.from("items").select().order("created_at"),
   {
@@ -17,6 +17,7 @@ const { data: items } = await useAsyncData(
           created_at: item.created_at,
         };
       }),
+    lazy: true,
   },
 );
 </script>
@@ -31,7 +32,18 @@ const { data: items } = await useAsyncData(
       <div
         class="xs:grid-cols-grid1 grid flex-grow gap-4 py-4 sm:grid-cols-grid2 md:grid-cols-grid3 lg:grid-cols-grid4"
       >
-        <ItemCard v-for="item in items" :key="item.id" :item="item" />
+        <div
+          v-if="pending"
+          v-for="n in 5"
+          class="flex max-w-[600px] flex-col gap-4"
+        >
+          <div class="skeleton h-36 w-full"></div>
+          <div class="skeleton h-4 w-28"></div>
+          <div class="skeleton h-4 w-full"></div>
+          <div class="skeleton h-4 w-full"></div>
+          <div class="skeleton h-4 w-full"></div>
+        </div>
+        <ItemCard v-else v-for="item in items" :key="item.id" :item="item" />
       </div>
     </div>
   </div>
