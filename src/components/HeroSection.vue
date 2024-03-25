19b4f4697c7a4c05d1eb7ff2ffcc6d9dcd5b0d1d
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-25 13:57:06 +0300

    add nowrap in wide buttons in the hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -68,13 +68,13 @@ const { data: cities } = await useAsyncData(
         </button>
       </form>
       <button
-        class="btn btn-sm btn-block flex items-center justify-start bg-base-100"
+        class="btn btn-sm btn-block flex flex-nowrap items-center justify-start bg-base-100"
       >
         <Icon name="material-symbols:box-edit-outline" size="1.2rem" />
         <span>{{ $t("Post Ad to Sell Used Second Hand Product") }}</span>
       </button>
       <button
-        class="btn btn-sm btn-block flex items-center justify-start bg-base-100"
+        class="btn btn-sm btn-block flex flex-nowrap items-center justify-start bg-base-100"
       >
         <Icon name="material-symbols:list-rounded" size="1.3rem" />
         <span>{{ $t("Find and Buy All Second Hand Products for Sale") }}</span>
@@ -82,7 +82,7 @@ const { data: cities } = await useAsyncData(
     </div>
     <div class="prose flex basis-1/2 flex-col gap-4 py-5 pl-4 pr-0">
       <p>{{ $t("Main Second Hand Product Categories") }}</p>
-      <div class="grid grid-cols-3 gap-2">
+      <div class="xs:grid-cols-2 grid grid-cols-3 gap-2">
         <div
           v-if="pending"
           v-for="key in 8"
@@ -92,7 +92,7 @@ const { data: cities } = await useAsyncData(
           v-else
           v-for="category in categories!"
           :key="category.id"
-          class="btn btn-outline btn-sm btn-block inline-flex flex-nowrap items-center justify-start"
+          class="btn btn-outline btn-sm btn-block inline-flex flex-nowrap items-center justify-start pr-0"
         >
           <Icon :name="category.icon!" size="1rem" />
           <span>{{ category.name }}</span>
