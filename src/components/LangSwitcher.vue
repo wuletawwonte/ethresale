796cce89f69cb258f025ba4935b84c4747a16372
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-25 13:58:39 +0300

    Fix category button wrapping issue (#122)
    * add nowrap css class to category buttons
    
    * add nowrap in wide buttons in the hero section
    
    * add nowrap to langswitcher

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -7,7 +7,7 @@ const { locale, locales, setLocale } = useI18n();
     <div
       tabindex="0"
       role="button"
-      class="btn btn-ghost btn-sm"
+      class="btn btn-ghost btn-sm flex-nowrap"
       aria-label="Language"
     >
       <icon name="ion:language" size="1.2rem" />
@@ -15,7 +15,7 @@ const { locale, locales, setLocale } = useI18n();
     </div>
     <div
       tabindex="0"
-      class="dropdown-content bg-base-200 text-base-content rounded-box top-px mt-16 max-h-[calc(100vh-10rem)] w-56 overflow-y-auto border border-white/5 shadow-2xl outline outline-1 outline-black/5"
+      class="dropdown-content top-px mt-16 max-h-[calc(100vh-10rem)] w-56 overflow-y-auto rounded-box border border-white/5 bg-base-200 text-base-content shadow-2xl outline outline-1 outline-black/5"
     >
       <ul class="menu menu-sm gap-1">
         <li v-for="lang in locales" :key="lang.key">
@@ -24,7 +24,7 @@ const { locale, locales, setLocale } = useI18n();
             :class="lang.code === locale ? 'active' : ''"
           >
             <span
-              class="badge badge-sm badge-outline !pl-1.5 !pr-1 pt-px font-mono !text-[.6rem] font-bold tracking-widest opacity-50 uppercase"
+              class="badge badge-outline badge-sm !pl-1.5 !pr-1 pt-px font-mono !text-[.6rem] font-bold uppercase tracking-widest opacity-50"
               >{{ lang.code }}</span
             >
             <span class="font-[sans-serif]">{{ lang.name }}</span>
