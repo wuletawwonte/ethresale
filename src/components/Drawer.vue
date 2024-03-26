5f0bbe4070a509d7847a4c207d665a0af77a9bfe
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-26 10:51:07 +0300

    add loggedin user in the drawer

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -13,8 +13,25 @@
         aria-label="close sidebar"
         class="drawer-overlay"
       ></label>
+
       <ul class="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
-        <!-- Sidebar content here -->
+        <div class="flex items-center gap-3">
+          <div class="avatar">
+            <div class="mask mask-squircle h-12 w-12">
+              <img
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                alt="Avatar Tailwind CSS Component"
+              />
+            </div>
+          </div>
+          <div>
+            <div class="font-bold">Hart Hagerty</div>
+            <div class="text-sm opacity-50">United States</div>
+          </div>
+        </div>
+
+        <div class="divider"></div>
+
         <li><a>Sidebar Item 1</a></li>
         <li><a>Sidebar Item 2</a></li>
       </ul>
