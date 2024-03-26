819848da996055d3c0f12979fe0947a44892aafd
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-26 10:23:21 +0300

    change cart icon and style the number (#125)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -51,24 +51,11 @@ const logout = async () => {
     </div>
     <div class="navbar-end gap-2">
       <ThemeSwitcher />
-      <div class="dropdown dropdown-end">
+      <div class="dropdown dropdown-end h-8">
         <div tabindex="0" role="button" class="btn btn-circle btn-ghost btn-sm">
           <div class="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <span class="badge indicator-item badge-sm">8</span>
+            <icon name="mdi:cart-outline" class="h-5 w-5" />
+            <span class="badge indicator-item badge-accent badge-sm">8</span>
           </div>
         </div>
         <div
