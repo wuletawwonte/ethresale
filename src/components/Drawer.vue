e9935efd4b80f84e5971a1cfdefa310c5f3fb63c
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-26 10:32:34 +0300

    Add content to the drawer component
    - create a separate SFC for the drawer
    - add menu content with user icon

---
+++ src/components/Drawer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div
+      class="drawer-content flex min-h-screen flex-col justify-between bg-base-200"
+    >
+      <slot></slot>
+    </div>
+
+    <div class="drawer-side z-10 md:hidden">
+      <label
+        for="my-drawer"
+        aria-label="close sidebar"
+        class="drawer-overlay"
+      ></label>
+      <ul class="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
