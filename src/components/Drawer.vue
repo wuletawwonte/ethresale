6cc19602a857093bc9d02f0a1621ff315acb5f83
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-27 05:45:05 +0300

    add active class when page active

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -32,31 +32,47 @@
 
         <div v-if="user" class="divider"></div>
 
-        <ul class="menu w-full rounded-box bg-base-200">
+        <ul class="menu w-full rounded-box bg-base-200 p-0">
           <li>
-            <nuxt-link to="/">
+            <nuxt-link to="/" :class="{ active: $route.path === '/' }">
               <icon name="fa-solid:home" class="h-5 w-5" />
               Home
             </nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/my-items">
+            <nuxt-link
+              to="/my-items"
+              :class="{ active: $route.path === '/my-items' }"
+            >
               <icon name="material-symbols:lists" class="h-5 w-5" />
               My items
             </nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/my-items/add">
-              <icon name="material-symbols:add-rounded" class="h-5 w-5" />
+            <nuxt-link
+              to="/my-items/add"
+              :class="{ active: $route.path === '/my-items/add' }"
+            >
+              <icon name="ic:outline-plus" class="h-5 w-5" />
               Add item
             </nuxt-link>
           </li>
 
           <li>
-            <nuxt-link to="/about"> About </nuxt-link>
+            <nuxt-link
+              to="/about"
+              :class="{ active: $route.path === '/about' }"
+            >
+              About
+            </nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/contact"> Contact </nuxt-link>
+            <nuxt-link
+              to="/contact"
+              :class="{ active: $route.path === '/contact' }"
+            >
+              Contact
+            </nuxt-link>
           </li>
         </ul>
 
@@ -64,14 +80,26 @@
 
         <div v-if="user">
           <li>
-            <a class="justify-between">
+            <nuxt-link to="#">
+              <icon name="material-symbols:account-circle" class="h-5 w-5" />
               Profile
               <span class="badge">New</span>
-            </a>
+            </nuxt-link>
           </li>
-          <li><a>Settings</a></li>
           <li>
-            <a @click.prevent="logout">Logout</a>
+            <nuxt-link to="#">
+              <icon
+                name="material-symbols-light:settings-rounded"
+                class="h-5 w-5"
+              />
+              Settings
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link @click.prevent="logout">
+              <icon name="tabler:logout-2" class="h-5 w-5" />
+              Logout
+            </nuxt-link>
           </li>
         </div>
       </ul>
