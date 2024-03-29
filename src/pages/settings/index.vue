f492be2d5de2457690a9c1e57705f1f28b0305dd
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-29 12:20:55 +0300

    add dark theme toggle template

--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -1,3 +1,38 @@
 <template>
-  <h2>Settings page</h2>
+  <div class="p-4">
+    <div
+      class="w-full max-w-md rounded-lg border border-gray-200 bg-base-100 p-4 shadow dark:border-gray-700 sm:p-8"
+    >
+      <div class="mb-4 flex items-center justify-between">
+        <h5
+          class="text-xl font-bold leading-none text-gray-900 dark:text-white"
+        >
+          Display settings
+        </h5>
+      </div>
+      <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+          <li class="py-3 sm:py-4">
+            <div class="flex items-center">
+              <div class="min-w-0 flex-1">
+                <p
+                  class="truncate text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Dark theme
+                </p>
+                <p class="truncate text-sm text-gray-500 dark:text-gray-400">
+                  turn on dark mode
+                </p>
+              </div>
+              <div
+                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+              >
+                <input type="checkbox" class="toggle" checked />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
