8a4176c005f52f72f5db2676922c1355aa618fd2
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-04-15 11:16:08 +0300

    pass itemModel value to ItemPreview component

---
+++ src/components/ItemPreview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script setup lang="ts">
+import type { ItemModel } from "@/types";
+
+const { item } = defineProps({
+  item: Object as PropType<ItemModel>,
+});
+
+console.log(item);
+// console.log(item)
+</script>
